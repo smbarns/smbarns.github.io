af735ce9b118a5967ca81416d3f3c93a2e56328f
Spencer Barns <35710408+smbarns@users.noreply.github.com>
2023-04-20 15:06:06 -0700

    encrypt work

---
+++ encrypt-it.js
@@ -0,0 +1,84 @@
+/*
+ * Starter file 
+ */
+(function() {
+  "use strict";
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window, signalling the HTML DOM has been constructed
+   * on the page. When this event occurs, the attached function (init) will be called.
+   */
+  window.addEventListener("load", init);
+  window.addEventListener('load', function() {
+    console.log('Window loaded!');
+  });
+  /**
+   * TODO: Write a function comment using JSDoc.
+   */
+  function init() {
+    const encryptItButton = document.getElementById("encrypt-it");
+    encryptItButton.addEventListener("click", handleButtonClickEncrypt);
+    changeFont();
+    toUpper();
+    // Note: In this function, we usually want to set up our event handlers
+    // for UI elements on the page.
+  
+  }
+
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+  function handleButtonClickEncrypt(event) {
+    const inputText = document.getElementById("input-text").value;
+    console.log("Button clicked!");
+    const encryptedText = shiftCipher(inputText);
+    const outputParagraph = document.getElementById("output");
+    changeFont(inputText);
+    toUpper(inputText);
+    outputParagraph.textContent = encryptedText;
+  }
+
+  function changeFont(text) {
+    const radioButtons = document.getElementsByName("text-size");
+    radioButtons.forEach(function(radioButton) {
+      radioButton.addEventListener("change", function(event) {
+        const fontSize = event.target.value;
+        const resultElement = document.getElementById("result");
+        resultElement.style.fontSize = fontSize;
+      });
+    });
+  }
+
+  function toUpper(text)  {
+    const allCapsCheckbox = document.getElementById("all-caps");
+    allCapsCheckbox.addEventListener("change", function(event) {
+      const isChecked = event.target.checked;
+      const resultElement = document.getElementById("result");
+      if (isChecked) {
+        resultElement.textContent = resultElement.textContent.toUpperCase();
+      } else {
+        resultElement.textContent = resultElement.textContent.toLowerCase();
+  }
+});
+  }
+
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      } else if (text[i] == 'z') {
+        result += 'a';
+      } else { // letter is between 'a' and 'y'
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
+    }
+    return result;
+  }
+
+
+})();
